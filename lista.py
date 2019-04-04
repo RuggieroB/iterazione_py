6868fa5e2a060cbfbadec10e220e39197d6b71d9
EnricoRuggieroB <1bmeccenricorugbucchianico@gmail.com>
2019-04-04 12:02:23 +0200

    Programmi di Laboratorio di Programmazione (Linguaggio Pyton (py)). Alunno: Bucchianico Enrico Ruggiero, 4^Finf.

--- lista.py
+++ lista.py
@@ -1,3 +1,5 @@
+'''
+
 # iterazione_py
 
 # Programmi di Laboratorio di Programmazione (Linguaggio "Pyton" ("py")). Alunno: Bucchianico Enrico Ruggiero, 4^Finf.
@@ -5,4 +7,12 @@
 
 - Programma "lista.py": Programma che data una lista di valori qualsiasi, ne stampa gli elementi uno per riga.
 
-- Programma "tabelline.py": Programma che stampa la Tavola Pitagorica da "0" a "12".
+'''
+
+lista=[10,25,45,'Ciao!',44.2,(8,45.30),'Di Provenza, il mar, il suol, chi dal cor ti cancello?']
+
+print("La lista memorizzata è\t",lista,"\n")
+
+print("Gli elementi della lista singolarmente sono:\n")
+for i in lista:
+	print(i)
---
+++ tabelline.py
@@ -0,0 +1,18 @@
+'''
+
+# iterazione_py
+
+# Programmi di Laboratorio di Programmazione (Linguaggio "Pyton" ("py")). Alunno: Bucchianico Enrico Ruggiero, 4^Finf.
+
+
+- Programma "tabelline.py": Programma che stampa la Tavola Pitagorica da "0" a "12".
+
+NON COMPLETO
+
+'''
+
+print("TAVOLA PITAGORICA DA \"0\" A \"12\"\n\n")
+
+for v in range(0, 12):
+	for o in range (0, 12):
+		print("%(o*v)","\t")
